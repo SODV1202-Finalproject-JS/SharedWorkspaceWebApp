b8a7159d531978bd85fef56bb6f32969b6351392
HectorBernstorff <95435499+HectorBernstorff@users.noreply.github.com>
2022-04-23 23:26:33 -0600

    Update
    Update CSS, Fix Style workspacelist and filteredworkspace

--- server.js
+++ server.js
@@ -388,5 +388,5 @@ app.get('/filteredworkspace', function(req, res){
 var server = app.listen(3005, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://localhost:3005")
+    console.log("http://localhost:3005/login")
 })
